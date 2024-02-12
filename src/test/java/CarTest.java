cb2a50e9b4c0413f03d8f1add2b693f5c04d6c30
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 15:34:10 +0000

    create test for Car class

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CarTest {
+
+    Car car;
+
+    @BeforeEach
+    public void setUp() {
+        car = new Car("Toyota Auris");
+    }
+
+    @Test
+    public void canStartEngine() {
+        assertThat(car.startEngine()).isEqualTo("VROOM I AM A CAR, THIS IS THE SOUND A CAR MAKES");
+    }
+}
