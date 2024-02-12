8e7b16e2b5ae2dac853fb8e21a2e12e8f93caa8a
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 16:05:19 +0000

    refactor order of getters

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -6,12 +6,13 @@ public class Car extends Vehicle {
         this.model = model;
     }
 
+    public String getModel() {
+        return this.model;
+    }
+
     @Override
     public String startEngine() {
         return "VROOM I AM A CAR, THIS IS THE SOUND A CAR MAKES";
     }
 
-    public String getModel() {
-        return this.model;
-    }
 }
