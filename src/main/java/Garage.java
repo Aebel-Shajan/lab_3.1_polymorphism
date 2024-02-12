c4d2cd143ef3c2c23a16c4df99cba837d7b6be56
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 16:48:14 +0000

    create Garage class

---
+++ src/main/java/Garage.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Garage {
+
+    private String owner;
+
+    private ArrayList<Vehicle> vehicles;
+
+    public Garage(String owner) {
+        this.owner = owner;
+        vehicles = new ArrayList<>();
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public int getVehicleCount() {
+        return this.vehicles.size();
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        this.vehicles.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+        this.vehicles.remove(vehicle);
+    }
+
+    public float calculateCollectiveValue() {
+        float totalValue = 0f;
+        for (Vehicle vehicle: vehicles) {
+            totalValue += vehicle.getValue();
+        }
+        return totalValue;
+    }
+
+
+
+}
