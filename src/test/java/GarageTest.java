62abbc014e781529d93a4db8a727fe70a01f667a
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 16:44:26 +0000

    create test for Garage Class

---
+++ src/test/java/GarageTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GarageTest {
+
+    Garage garage;
+
+    @BeforeEach
+    public void setUp(){
+        garage = new Garage("Aebel");
+    }
+
+    @Test
+    public void canGetOwner() {
+        assertThat(garage.getOwner()).isEqualTo("Aebel");
+    }
+
+    @Test
+    public void canSetOwner() {
+        garage.setOwner("Bank");
+        assertThat(garage.getOwner()).isEqualTo("Bank");
+    }
+
+    @Test
+    public void canGetVehicleCount() {
+        assertThat(garage.getVehicleCount()).isEqualTo(0);
+    }
+
+    @Test
+    public void canAddVehicle() {
+        Car lambo = new Car(2, 10000, "Lamborghini Huracan");
+        garage.addVehicle(lambo);
+        assertThat(garage.getVehicleCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void canRemoveVehicle() {
+        Car lambo = new Car(2, 10000, "Lamborghini Huracan");
+        Car auris = new Car(5, 1000, "Toyota Auris");
+        garage.addVehicle(auris);
+        garage.addVehicle(lambo);
+        garage.removeVehicle(lambo);
+        assertThat(garage.getVehicleCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void canCalculateCollectiveValue() {
+        Car lambo = new Car(2, 10000, "Lamborghini Huracan");
+        Car auris = new Car(5, 1000, "Toyota Auris");
+        garage.addVehicle(auris);
+        garage.addVehicle(lambo);
+        assertThat(garage.calulateCollectiveValue()).isEqualTo(11000);
+    }
+}
