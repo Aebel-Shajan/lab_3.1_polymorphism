e53ec3d416734a27a8f51ae6c61398c9593a1648
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 15:48:31 +0000

    create car classe which extends vehicle

---
+++ src/main/java/Car.java
@@ -0,0 +1,13 @@
+public class Car extends Vehicle {
+
+    String model;
+    public Car(int maximumOccupancy, float value, String model) {
+        super(4, maximumOccupancy, value);
+        this.model = model;
+    }
+
+    @Override
+    public String startEngine() {
+        return "VROOM I AM A CAR, THIS IS THE SOUND A CAR MAKES";
+    }
+}
