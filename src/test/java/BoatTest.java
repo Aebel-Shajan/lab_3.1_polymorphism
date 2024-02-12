fea291d503748f23829e8c977a93ba28a8281c8e
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 16:09:50 +0000

    create tests for boat

---
+++ src/test/java/BoatTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BoatTest {
+    Boat boat;
+
+    @BeforeEach
+    public void setUp() {
+        boat = new Boat(6, 5000f, "sailboat");
+    }
+
+    @Test
+    public void canGetNumberOfWheels() {
+        assertThat(boat.getNumberOfWheels()).isEqualTo(4);
+    }
+
+    @Test
+    public void canGetMaximumOccupancy() {
+        assertThat(boat.getMaximumOccupancy()).isEqualTo(5);
+    }
+
+    @Test
+    public void canGetValue() {
+        assertThat(boat.getValue()).isEqualTo(1000f);
+    }
+
+    @Test
+    public void canSetValue() {
+        boat.setValue(2000f);
+        assertThat(boat.getValue()).isEqualTo(2000f);
+    }
+
+    @Test
+    public void canGetType() {
+        assertThat(boat.getType()).isEqualTo("sailboat");
+    }
+
+    @Test
+    public void canStartEngine() {
+        assertThat(boat.startEngine()).isEqualTo("SWOOSH, THIS IS BOAT SOUND");
+    }
+
+    @Test
+    public void canSail(){
+        assertThat(boat.sail()).isEqualTo("shhhshhhshhh");
+    }
+
+
+
+}
