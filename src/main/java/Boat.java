a0b137d20bf3d337a19eb1315fa7101980222b67
Aebel Shajan <aebelshajan@gmail.com>
2024-02-12 16:14:19 +0000

    create Boat class

---
+++ src/main/java/Boat.java
@@ -0,0 +1,21 @@
+public class Boat extends Vehicle {
+    private String type;
+
+    public Boat(int maximumOccupancy, float value, String type) {
+        super(0, maximumOccupancy, value);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String startEngine() {
+        return "SWOOSH, THIS IS BOAT SOUND";
+    }
+
+    public String sail() {
+        return "shhhshhhshhh";
+    }
+}
