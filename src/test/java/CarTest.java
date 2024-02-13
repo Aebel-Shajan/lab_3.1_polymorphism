0304d2c44bccefcd71890c4a35e1dec0c14b4254
Aebel Shajan <aebelshajan@gmail.com>
2024-02-13 14:43:00 +0000

    imported package models

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,3 +1,5 @@
+import models.Car;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/GarageTest.java
+++ src/test/java/GarageTest.java
@@ -1,3 +1,4 @@
+import models.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
