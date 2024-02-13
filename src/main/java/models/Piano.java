66a9550e9e65e86c2dc363a1b65395231c06e794
Aebel Shajan <aebelshajan@gmail.com>
2024-02-13 15:21:40 +0000

    overload playPiano with playPiano(song, duration)

--- src/main/java/models/Piano.java
+++ src/main/java/models/Piano.java
@@ -22,4 +22,8 @@ public class Piano implements IGetValue {
         return "now playing: " + song;
     }
 
+    public String playPiano(String song, double duration) {
+        return "now playing: " + song + " for " + duration + " seconds";
+    }
+
 }
