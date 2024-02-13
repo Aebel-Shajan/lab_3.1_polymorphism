0ef30cf507c7ec1eae5d25121914ea52c68bf26d
Aebel Shajan <aebelshajan@gmail.com>
2024-02-13 15:26:57 +0000

    refactor garage to use valuables instead of vehicles keyword

--- src/main/java/models/Garage.java
+++ src/main/java/models/Garage.java
@@ -1,42 +1,44 @@
 package models;
 
+import interfaces.IGetValue;
+
 import java.util.ArrayList;
 
 public class Garage {
 
     private String owner;
 
-    private ArrayList<Vehicle> vehicles;
+    private ArrayList<IGetValue> valuables;
 
     public Garage(String owner) {
         this.owner = owner;
-        vehicles = new ArrayList<>();
+        valuables = new ArrayList<>();
     }
 
     public String getOwner() {
         return this.owner;
     }
 
-    public int getVehicleCount() {
-        return this.vehicles.size();
+    public int getValuableCount() {
+        return this.valuables.size();
     }
 
     public void setOwner(String owner) {
         this.owner = owner;
     }
 
-    public void addVehicle(Vehicle vehicle) {
-        this.vehicles.add(vehicle);
+    public void addValuable(IGetValue valuable) {
+        this.valuables.add(valuable);
     }
 
-    public void removeVehicle(Vehicle vehicle) {
-        this.vehicles.remove(vehicle);
+    public void removeValuable(IGetValue valuable) {
+        this.valuables.remove(valuable);
     }
 
     public float calculateCollectiveValue() {
         float totalValue = 0f;
-        for (Vehicle vehicle: vehicles) {
-            totalValue += vehicle.getValue();
+        for (IGetValue valuable: this.valuables) {
+            totalValue += valuable.getValue();
         }
         return totalValue;
     }
