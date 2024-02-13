b82409b5a6a4bdd9cadd6dcf4e1ff14235362eae
Aebel Shajan <aebelshajan@gmail.com>
2024-02-13 15:11:21 +0000

    Create test piano.canGetValue

--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -2,11 +2,18 @@ import models.Piano;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PianoTest {
     Piano piano;
 
     @BeforeEach
     public void setUp() {
-        piano = new Piano(100);
+        piano = new Piano(100f);
+    }
+
+    @Test
+    public void canGetValue() {
+        assertThat(piano.getValue()).isEqualTo(100f);
     }
 }
